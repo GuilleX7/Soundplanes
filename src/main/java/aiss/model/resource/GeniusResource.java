7bad685103920e1a05d35b351d15a742d470914f
CarmenMariaMP <62175263+CarmenMariaMP@users.noreply.github.com>
2020-05-21 19:45:35 +0200

    Avance en las pruebas

--- src/main/java/aiss/model/resource/GeniusResource.java
+++ src/main/java/aiss/model/resource/GeniusResource.java
@@ -1,6 +1,7 @@
 package aiss.model.resource;
 
 import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
--- src/test/java/aiss/model/resource/GeniusResource_JUnitTest.java
+++ src/test/java/aiss/model/resource/GeniusResource_JUnitTest.java
@@ -1,5 +1,33 @@
 package aiss.model.resource;
 
-public class GeniusResource_JUnitTest {
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+import org.junit.Test;
+
+import aiss.model.genius.Song;
+
+public class GeniusResource_JUnitTest {
+	
+	@Test
+	public void searchSongTest() throws UnsupportedEncodingException {
+		String title="Bohemian Rhapsody";
+		GeniusResource song= new GeniusResource();
+		List<Song> songResults= song.searchSong(title);
+		
+		assertNotNull("The search returned null", songResults);
+		assertFalse("There is no song called: "+title,  songResults.isEmpty());
+	}
+	
+	@Test
+	public void getLyricsFromSongTest() throws IOException {
+		Song s= new Song();
+		GeniusResource song= new GeniusResource();
+		String lyricsResults=song.getLyricsFromSong(s);
+		assertNotNull("The search returned null", lyricsResults);
+	}
 }
